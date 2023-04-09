32c9d117144ce60d31bf34c1aaa45fa9b4a6b057
Jack Blonigen <jack.je.blonigen@gmail.com>
2023-04-09 18:52:51 -0500

    added and removed food/animals plus logged

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -49,8 +49,8 @@ animalArray.push('penguin');
 console.log('Added an animal to end,', animalArray);
 
 // 4.a. TODO: Add a new food at the end of your array & log the array
-
-
+favoriteFoods.push('Seafood');
+console.log(favoriteFoods);
 
 // Example: Remove the last animal by using Array.pop
 let removedAnimal = animalArray.pop();
@@ -59,7 +59,9 @@ console.log('The animals are now', animalArray);
 
 // 4.b. TODO: Remove the food at the end of your array & 
 //      log both the food removed and the updated array
-
+let removedFood = favoriteFoods.pop();
+console.log('Removed the last favorite food', removedFood);
+console.log('The favorite foods are now', favoriteFoods);
 
 // Example: Add an animal to the beginning using Array.unshift
 animalArray.unshift('walrus');
